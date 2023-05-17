b272dab7f1141a3d0af060c4835122b87bc9d8c6
Mikail ÖZTÜRK <75627147+MIKAIL-OZTURK@users.noreply.github.com>
2023-05-17 08:34:45 +0300

    Update socketcan.h

--- socketcan.h
+++ socketcan.h
@@ -1,7 +1,6 @@
 #ifndef SOCKETCAN_H
 #define SOCKETCAN_H
 
-//#include <QObject>
 #include <QTimer>
 #include <QCanBusFrame>
 #include <QCanBusDevice>
@@ -10,20 +9,12 @@
 class SocketCan : public QObject
 {
 	Q_OBJECT
-	Q_PROPERTY(int canId READ canId WRITE setCanId MEMBER m_canId NOTIFY canIdChanged)
 	Q_PROPERTY(CanBusDeviceState deviceState READ deviceState NOTIFY deviceStateChanged)
 	Q_PROPERTY(CanBusStatus busStatus READ busStatus NOTIFY busStatusChanged)
 
 public:
 	explicit SocketCan(QObject *parent = nullptr);
 	~SocketCan();
-
-	enum FrameType {
-		DataFrame = QCanBusFrame::DataFrame,
-		ErrorFrame = QCanBusFrame::ErrorFrame,
-		RemoteRequestFrame = QCanBusFrame::RemoteRequestFrame
-	};
-	Q_ENUM(FrameType)
 
 	enum CanBusDeviceState {
 		UnconnectedState,
@@ -42,15 +33,14 @@ public:
 	};
 	Q_ENUM(CanBusStatus)
 
-	int canId()const;
-	void setCanId(int id);
 	bool isConnected() const;
 	CanBusDeviceState deviceState() const;
 	CanBusStatus busStatus() const;
 
 signals:
-	void dataReceived(const QString& message);
-	void canIdChanged();
+
+signals:
+	void dataReceived(QString ID, const QString& message);
 	void deviceStateChanged();
 	void busStatusChanged();
 
@@ -58,16 +48,16 @@ public slots:
 	void connectSocket();
 	void bindSocket();
 	void readData();
-	void sendData(const QString &message);
+	void sendData(const QString &ID, const QString &message);
 	void disconnectSocket();
 
 private:
-	int m_canId;
 	int m_canSocket;
 	QTimer m_timer;
 	bool m_socketCreated;
 	CanBusDeviceState m_deviceState;
 	CanBusStatus m_busStatus;
+	QString lastID;
 };
 
 #endif // SOCKETCAN_H
